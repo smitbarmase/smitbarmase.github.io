b6b62210d8ab5366ea7f8f905978de12a087e70d
Smit Barmase <smitbarmase@outlook.com>
2020-12-21 01:28:41 +0530

    config

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import Button from "./Button";
 
 import { DiGithubBadge } from "react-icons/di";
@@ -19,9 +17,7 @@ const Card: React.FC<CardProps> = (props) => {
     <div className="bg-white rounded shadow overflow-hidden transition-shadow hover:shadow-md">
       <div className="p-3 space-y-3">
         <div className="flex space-x-3">
-          <div className="relative h-28 w-28 rounded overflow-hidden">
-            <Image src={img} alt={title} layout="fill" />
-          </div>
+          <img src={img} alt={title} className="h-28 w-28 rounded" />
           <div className="flex-1 flex flex-col justify-between">
             <div className="space-y-1">
               <div className="font-medium text-sm">{title}</div>
